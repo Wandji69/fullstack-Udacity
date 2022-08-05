a5635b67caa1eac82b628ffc9f6c3803329d1115
Wandji69 <collinschuwa@gmail.com>
2022-08-05 16:13:00 +0100

    demo todo with template html.

---
+++ DemoTodo/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://collins:postgres.01@localhost:5432/examples'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+
+def __repr__(self):
+    return f'<Todo {self.id} {self.description}>'
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', data=Todo.query.all())
+
+
+@app.route('/todo/create')
+def create():
+    description = request.form.get('description')
+
+
+# always include this at the bottom of your code (port 3000 is only necessary in workspaces)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
